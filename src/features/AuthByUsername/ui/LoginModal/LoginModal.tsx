18eeac9a6b3c5675a5bbc35aad7d117ce8c45922
Daniil Faershtein <boblon12@mail.ru>
2023-03-03 22:17:27 +0300

    4.2 add lazy modal add input

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,7 +1,5 @@
-import { classNames } from 'shared/lib/classNames/classNames';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { LoginForm } from '../LoginForm/LoginForm';
-import cls from './LoginModal.module.scss';
 
 interface LoginModalProps {
   className?: string;
@@ -9,14 +7,9 @@ interface LoginModalProps {
   onClose: () => void;
 }
 
-export const LoginModal = ({ className, onClose, isOpen }: LoginModalProps) => {
+export const LoginModal = ({ onClose, isOpen }: LoginModalProps) => {
   return (
-    <Modal
-      lazy
-      onClose={onClose}
-      isOpen={isOpen}
-      className={classNames(cls.loginModal, {}, [className])}
-    >
+    <Modal lazy onClose={onClose} isOpen={isOpen}>
       <LoginForm />
     </Modal>
   );
